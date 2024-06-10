2d2fd86c370c78d4d4a2625626141ddc2b3b1005
Kimy474 <kimberlyduran474@gmail.com>
2024-06-09 23:33:55 -0400

    parrafo

---
+++ parrf/parrafo.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, por favor escribe algo de tu preferencia");
+string texto = Console.ReadLine();
+
+string[] par = texto.Split("");
+int frecuencia = 0;
+Dictionary<string, int> almacen = new Dictionary<string, int>() { };
+
+
+foreach (var item in par) 
+{
+    if (almacen.ContainsKey(item))
+    {
+        almacen[item]++;
+        frecuencia++;
+    }
+    else
+    {
+        almacen[item] = 1;
+    }
+}
+
+foreach (var i in almacen)
+{
+    Console.WriteLine($"la frase : {i.Key}, y su frecuencia es:  {i.Value}");
+}
